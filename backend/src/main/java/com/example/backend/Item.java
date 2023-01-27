3181f2e4cee43bccf7b35da19164cff58acbe9e8
abderrahman504 <rahmankhsherb@gmail.com>
2023-01-27 18:45:18 +0200

    Fixed more bugs

--- backend/src/main/java/com/example/backend/Item.java
+++ backend/src/main/java/com/example/backend/Item.java
@@ -7,9 +7,9 @@ public class Item {
     private Color color;
     public Item(){
         Random rand = new Random();
-        int r = rand.nextInt() % 256;
-        int g = rand.nextInt() % 256;
-        int b = rand.nextInt() % 256;
+        int r = rand.nextInt(0, 255);
+        int g = rand.nextInt(0, 255);
+        int b = rand.nextInt(0, 255);
         color = new Color(r, g, b);
     }
 
